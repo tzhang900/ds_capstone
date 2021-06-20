284f191e8a0546dda146dedd389f70f63e384575
tzhang900 <bluepart333@gmail.com>
2021-06-20 15:22:50 -0700

    stage check in

--- heroku/app.py
+++ heroku/app.py
@@ -18,7 +18,9 @@ sourceurl = 'https://www.kaggle.com/xtyscut/video-games-sales-as-at-22-dec-2016c
 githublink = 'https://github.com/tzhang900/ds_capstone/tree/main/heroku'
 genre_options = ['Sports', 'Platform', 'Racing', 'Role-Playing', 'Puzzle', 'Misc', 'Shooter', 'Simulation', 'Action', 'Fighting', 'Adventure', 'Strategy']
 platform_options = ['Wii', 'NES', 'GB', 'DS', 'X360', 'PS3', 'PS2', 'SNES', 'GBA', 'PS4', '3DS', 'N64', 'PS', 'XB', 'PC', '2600', 'PSP', 'XOne', 'WiiU', 'GC', 'GEN', 'DC', 'PSV', 'SAT', 'SCD', 'WS', 'NG', 'TG16', '3DO', 'GG', 'PCFX']
-
+image1='characters.jpg'
+image2='consoles.jpg'
+background='background.jpg'
 ########## Set up the chart
 
 import pandas as pd
@@ -76,30 +78,39 @@ lin_reg_sales.fit(X_sales_train, y_sales_train)
 
 ########### Set up the layout
 
-app.layout = html.Div(children=[
-    html.H1('Game Sales in Euro Prediction'),
-    html.Div([
+app.layout = html.Div(
+    style={
+    },
+    children=[
+        html.H1('Game Sales in Euro Prediction'),
         html.Div([
-                html.H6("Copies Sold in North America in millions, eg 3.3"),
-                dcc.Input(id='na-sales', type="number"),
-                html.H6('Select Genre:'),
-                dcc.Dropdown(
-                    id='genres-drop',
-                    options=[{'label': i, 'value': i} for i in genre_options],
-                    value='genre'
-                ),
-                html.H6('Select Platform:'),
-                dcc.Dropdown(
-                    id='platforms-drop',
-                    options=[{'label': i, 'value': i} for i in platform_options],
-                    value='platform'
-                ),
-        ], className='four columns'),
-        html.Div(id='your-output-here', children='',  style={'color': 'blue', 'fontSize': 40}, className='eight columns'),
-    ], className='twelve columns'),
-    html.A('Code on Github', href=githublink),
-    html.Br(),
-    html.A("Data Source", href=sourceurl),
+            html.Div([
+                    html.H6("Copies Sold in North America in millions, eg 3.3"),
+                    dcc.Input(id='na-sales', type="number"),
+                    html.H6('Select Genre:'),
+                    dcc.Dropdown(
+                        id='genres-drop',
+                        options=[{'label': i, 'value': i} for i in genre_options],
+                        value='genre'
+                    ),
+                    html.H6('Select Platform:'),
+                    dcc.Dropdown(
+                        id='platforms-drop',
+                        options=[{'label': i, 'value': i} for i in platform_options],
+                        value='platform'
+                    ),
+                    html.Img(src=app.get_asset_url(image2), style={'width': '100%', 'height': '10%'}),
+            ], className='four columns'),
+            html.Div([
+                html.Div(id='your-output-here', children='',  style={'color': 'blue', 'fontSize': 40}),
+                html.Img(src=app.get_asset_url(image1), style={'width': '80%', 'height': '10%'}),
+            ], className='eight columns'),
+        ], className='twelve columns'),
+        html.P(),
+        html.Br(),
+        html.A('Code on Github', href=githublink),
+        html.Br(),
+        html.A("Data Source", href=sourceurl),
     ]
 )
 
