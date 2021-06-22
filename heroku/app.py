02c4df60ed6d3d83796555ead7ea83c4c754c0da
tzhang900 <bluepart333@gmail.com>
2021-06-21 21:27:42 -0700

    Update with Pickle

--- heroku/app.py
+++ heroku/app.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output, State
 
+import pickle
 import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
@@ -18,10 +19,23 @@ sourceurl = 'https://www.kaggle.com/xtyscut/video-games-sales-as-at-22-dec-2016c
 githublink = 'https://github.com/tzhang900/ds_capstone/tree/main/heroku'
 genre_options = ['Sports', 'Platform', 'Racing', 'Role-Playing', 'Puzzle', 'Misc', 'Shooter', 'Simulation', 'Action', 'Fighting', 'Adventure', 'Strategy']
 platform_options = ['Wii', 'NES', 'GB', 'DS', 'X360', 'PS3', 'PS2', 'SNES', 'GBA', 'PS4', '3DS', 'N64', 'PS', 'XB', 'PC', '2600', 'PSP', 'XOne', 'WiiU', 'GC', 'GEN', 'DC', 'PSV', 'SAT', 'SCD', 'WS', 'NG', 'TG16', '3DO', 'GG', 'PCFX']
+region_options = ['Japan', 'Europe', 'Other']
 image1='characters.jpg'
 image2='consoles.jpg'
 background='background.jpg'
-########## Set up the chart
+
+### open pickle files
+filename = open('model_components/na_eu_lr_model.pkl', 'rb')
+lr_eu=pickle.load(filename)
+filename.close()
+
+filename = open('model_components/na_jp_lr_model.pkl', 'rb')
+lr_jp=pickle.load(filename)
+filename.close()
+
+filename = open('model_components/na_other_lr_model.pkl', 'rb')
+lr_other=pickle.load(filename)
+filename.close()
 
 import pandas as pd
 games = pd.read_csv('assets/Video_Games.csv')
@@ -82,22 +96,28 @@ app.layout = html.Div(
     style={
     },
     children=[
-        html.H1('Game Sales in Euro Prediction'),
+        html.H1('Game Sales by Region Prediction'),
         html.Div([
             html.Div([
+                    html.H6('Region to Predict:'),
+                    dcc.Dropdown(
+                        id='regions-drop',
+                        options=[{'label': i, 'value': i} for i in region_options],
+                        value='Europe'
+                    ),
                     html.H6("Copies Sold in North America in millions, eg 3.3"),
                     dcc.Input(id='na-sales', type="number"),
                     html.H6('Select Genre:'),
                     dcc.Dropdown(
                         id='genres-drop',
                         options=[{'label': i, 'value': i} for i in genre_options],
-                        value='genre'
+                        value='Sports'
                     ),
                     html.H6('Select Platform:'),
                     dcc.Dropdown(
                         id='platforms-drop',
                         options=[{'label': i, 'value': i} for i in platform_options],
-                        value='platform'
+                        value='Wii'
                     ),
                     html.Img(src=app.get_asset_url(image2), style={'width': '100%', 'height': '10%'}),
             ], className='four columns'),
@@ -125,19 +145,26 @@ def listToString(f):
     
 # make a function that can intake any varname and produce a map.
 @app.callback(Output('your-output-here', 'children'),
-             [Input('na-sales', 'value'),
+             [Input('regions-drop', 'value'),
+              Input('na-sales', 'value'),
               Input('genres-drop', 'value'),
               Input('platforms-drop', 'value')])
-def make_figure(sales,genre,platform):
-    if (sales is None or genre is None or genre == "genre" or platform is None or platform == "platform"):
+def make_figure(region, sales,genre,platform):
+    if (sales is None or genre is None or genre == " " or platform is None or platform == " " or region is None):
        return f'Please enter all values to predict the result'
     int_genre = list(label_genre).index(genre)
     int_platform = list(label_platform).index(platform)
     mygraphtitle = f'Exports of {genre} and {platform} in 2011'
     mycolorscale = f'genre {int_genre} and platform {int_platform}'
     input_value = [[int_genre, int_platform, sales]]
-    prediction = lin_reg_sales.predict(input_value)
-    return f'Predict {platform} {genre} game sales in Europe:' +   listToString(prediction)
+    #prediction = lin_reg_sales.predict(input_value)
+    if region == "Europe":
+        prediction = lr_eu.predict(input_value)
+    elif region == "Japan":
+        prediction = lr_jp.predict(input_value)
+    else:
+        prediction = lr_other.predict(input_value)
+    return f'Predict {platform} {genre} game sales in {region}: ' +   listToString(prediction) + " million"
     #return  mycolorscale
     #return mygraphtitle
 
